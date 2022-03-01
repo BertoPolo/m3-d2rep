79510903a829197f99fb71101a4cd974cb226c0a
BertoPolo <bertopolo91@gmail.com>
2022-03-01 15:24:56 +0100

    a

--- AlbumPage.js
+++ AlbumPage.js
@@ -25,24 +25,30 @@ mouseoverSong();
 // m3-d2rep
 const bandsPlace = document.getElementById("bandsPlace");
 
-const loadBands = () => {
-  fetch("https://striveschool-api.herokuapp.com/api/deezer/search?q=eminem")
+const loadBands = (band) => {
+  fetch("https://striveschool-api.herokuapp.com/api/deezer/search?q=" + band)
     .then((response) => {
       return response.json();
     })
     .then((data) => {
       console.log(data);
       console.log(data.data[0].title);
+      const realData = data.data;
 
-      /*  data.forEach((element) => {
-        bandsPlace.innerHTML += `<div class="col"> ${data.title}</div>`;
-      }); */
+      realData.forEach((element) => {
+        bandsPlace.innerHTML += `
+        <div class="card" style="width: 18rem;">
+  <img src="${element.album.cover}" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">${element.title}</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+        `;
+      });
     })
     .catch((error) => {
       console.log(error);
     });
-};
-
-window.onload = function () {
-  loadBands();
 };
